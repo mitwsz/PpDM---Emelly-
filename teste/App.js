e3dfcf49c37bf0bcb65fe015fbf56d0a2bf2ebce
mitwsz <emelly.pessanha@gsuite.iff.edu.br>
2023-11-10 15:06:55 -0300

    Começando meu app

--- teste/App.js
+++ teste/App.js
@@ -1,13 +1,43 @@
 //tipo o require
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function TelaInicial(){
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text>Meu primeiro app no react native! hello world!</Text>
+      <Button title = "CADASTRAR" onPress={()=>{
+          navigation.navigate('Cadastro')
+      }}></Button>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function TelaCadastro(){
+  return (
+    <View style={styles.container}>
+      <Text>Vamos fazer o cadastro!</Text>
+      <Button title = "VOLTAR"onPress={()=>{
+          navigation.navigate('Início')
+      }}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Início" component={TelaInicial} />
+        <Stack.Screen name="Cadastro" component={TelaCadastro} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
