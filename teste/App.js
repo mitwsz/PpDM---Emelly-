b0397e0cdc287b9ad886d043a5bc4b692782fc16
mitwsz <emelly.pessanha@gsuite.iff.edu.br>
2023-11-21 09:30:28 -0300

    Mais CRUDs

--- teste/App.js
+++ teste/App.js
@@ -29,6 +29,78 @@ function TelaCadastro(){
   );
 }
 
+function TelaLogin(){
+    return (
+      <View style={styles.container}>
+        <Text>Vamos fazer o login!</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Cadastro')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function RecuperarSenha(){
+    return (
+      <View style={styles.container}>
+        <Text>Vamos recuperar sua senha!</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Início')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function TelaInicioApp(){
+    return (
+      <View style={styles.container}>
+        <Text>Tela de início do app</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Início')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function TelaObras(){
+    return (
+      <View style={styles.container}>
+        <Text>Aqui tá as obras dos artistas</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Início')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function TelaArtista(){
+    return (
+      <View style={styles.container}>
+        <Text>Aqui tá a pintura e o artista</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Início')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function TelaPerfil(){
+    return (
+      <View style={styles.container}>
+        <Text>Aqui fica o perfil do usuário</Text>
+        <Button title = "VOLTAR"onPress={()=>{
+            navigation.navigate('Início')
+        }}></Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
@@ -36,6 +108,12 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Início" component={TelaInicial} />
         <Stack.Screen name="Cadastro" component={TelaCadastro} />
+        <Stack.Screen name="Login" component={TelaLogin} />
+        <Stack.Screen name="Recuperar senha" component={RecuperarSenha} />
+        <Stack.Screen name="Início" component={TelaInicioApp} />
+        <Stack.Screen name="Obras" component={TelaObras} />
+        <Stack.Screen name="Artista" component={TelaArtista} />
+        <Stack.Screen name="Perfil" component={TelaPerfil} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -44,7 +122,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8096AF',
     alignItems: 'center',
     justifyContent: 'center',
   },
